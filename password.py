679880b0680c5273947f2214f5a58231a782d2bd
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-07-21 13:15:16 +0300

    created class method to display the user information

--- password.py
+++ password.py
@@ -20,3 +20,44 @@ class user :
         delete_user method deletes the user info from user_list
         '''
         User.user_list.remove(self)
+        @classmethod
+    def find_by_account(cls,account):
+        '''
+        Method takes in account name and displays user info for that particular account
+        Args:
+            Account name to search for
+        Returns:
+            User info for that account
+        '''
+        for details in cls.user_list:
+            if details.account == account:
+                return details
+
+
+    @classmethod
+    def user_exists(cls,account):
+        '''
+        Method checks if user exists from the user_list
+        Args:
+            account : Account to search if user exists from user_list
+        Return:
+            Boolean: True or false depending if the user exists
+        '''
+
+        for user in cls.user_list:
+            if user.account == account:
+                return True
+
+        return False
+
+    @classmethod
+    def display_users(cls):
+        '''
+        Method that displays all users
+        '''
+        return cls.user_list
+
+    @classmethod
+    def copy_paswad(cls, account):
+        user_found = User.find_by_account(account)
+        pyperclip.copy(user_found.username)
