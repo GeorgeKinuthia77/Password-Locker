60b548b35b253a911c3ed2482a7f7123b2958129
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-07-21 14:43:25 +0300

    imported unnitest

--- password_test.py
+++ password_test.py
@@ -1,24 +1,3 @@
 import unittest
 from password import User
 from password import Credentials
-
-class TestUser(unittest.TestCase):
-    '''
-    Test class that defines tests for User class behaviours
-    Args:
-        unittest.TestCase: TestCase class that helps create test cases
-    '''
-
-    def setUp(self):
-        '''
-        Set up method to run before each test instance
-        '''
-        self.new_user = User("GeorgeKinuthia77","Instagram","Buru_klyn_niccur")# Instance of class User
-
-    def test_init(self):
-        '''
-        Test case to test if object is initialised properly
-        '''
-        self.assertEqual(self.new_user.username, "GeorgeKinuthia77")
-        self.assertEqual(self.new_user.account, "Instagram")
-        self.assertEqual(self.new_user.password, "Buru_klyn_niccur")
