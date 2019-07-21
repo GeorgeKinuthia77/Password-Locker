f9d7222470179097777f05d9edd7b273fd177e67
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-07-21 22:08:35 +0300

    created function tp create password and save a user

--- run.py
+++ run.py
@@ -11,3 +11,16 @@ def create_user(username, account,password):
     '''
     new_user = User(username, account,password)
     return new_user
+    
+def create_paswad(account, password):
+    '''
+    Function to create new password
+    '''
+    new_pass = Credentials(account, password)
+    return new_pass
+
+def save_user(user):
+    '''
+    Function to save users
+    '''
+    user.save_user()
