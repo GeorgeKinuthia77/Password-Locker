8a399bc55ac3d09fe134c4bbf2d92622bda987ab
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-07-21 13:06:53 +0300

    created file password.py and generated a class user

---
+++ password.py
@@ -0,0 +1,22 @@
+import random
+
+class user :
+    '''
+    Class that generates new instances of a User
+    '''
+    user_list = []
+    def __init__(self, username, account,password):
+        self.username = username
+        self.account = account
+        self.paswad=paswad
+
+    def save_user(self):
+        '''
+        save_user method saves user names into the user list
+        '''
+        User.user_list.append(self)
+    def delete_user(self):
+        '''
+        delete_user method deletes the user info from user_list
+        '''
+        User.user_list.remove(self)
